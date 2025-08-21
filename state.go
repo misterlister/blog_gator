77012bfbbaf7219c0ae85e7ee57c4e9b3794efed
misterlister <haydenglister@gmail.com>
2025-08-21 12:46:39 -0700

    Added db to state struct

--- state.go
+++ state.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/misterlister/blog_gator/internal/config"
+	"github.com/misterlister/blog_gator/internal/database"
 )
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
