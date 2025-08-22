dcb4d11d169db87b7fb7d4858e50564d650df217
misterlister <haydenglister@gmail.com>
2025-08-22 15:00:43 -0700

    Added commands to follow a feed, and get a list of followed feeds

---
+++ handler_follow.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/misterlister/blog_gator/internal/database"
+)
+
+func handlerFollow(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return errors.New("please provide the URL of the feed to follow")
+	}
+
+	if len(cmd.args) > 1 {
+		return errors.New("too many arguments provided")
+	}
+
+	feedURL := cmd.args[0]
+	feed, err := s.db.GetFeedByURL(context.Background(), feedURL)
+
+	if err != nil {
+		return err
+	}
+
+	username := s.cfg.CurrentUserName
+	user, err := s.db.GetUserByName(context.Background(), username)
+
+	if err != nil {
+		return err
+	}
+
+	params := database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	}
+
+	_, err = s.db.CreateFeedFollow(context.Background(), params)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s is now following %s\n", username, feed.Name)
+
+	return nil
+}
+
+func handlerFollowing(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return errors.New("too many arguments provided")
+	}
+
+	username := s.cfg.CurrentUserName
+	user, err := s.db.GetUserByName(context.Background(), username)
+
+	if err != nil {
+		return err
+	}
+
+	feedList, err := s.db.GetFeedFollowsForUser(context.Background(), user.ID)
+
+	if err != nil {
+		return err
+	}
+
+	if len(feedList) == 0 {
+		fmt.Printf("%s is is following no feeds\n", username)
+		return nil
+	}
+
+	fmt.Printf("%s is following:\n", username)
+
+	for _, feed := range feedList {
+		fmt.Printf(" - %s\n", feed.FeedName)
+	}
+	return nil
+}
