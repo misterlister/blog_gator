0b0f00295699719658dd94f73bfb6fe755b98874
misterlister <haydenglister@gmail.com>
2025-08-22 22:08:50 -0700

    Updated agg command to initiate repeated fetching and collecting of updated feeds

--- handler_agg.go
+++ handler_agg.go
@@ -4,20 +4,56 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	if len(cmd.args) != 0 {
+	if len(cmd.args) == 0 {
+		return errors.New("no timeBetweenRequests value was passed")
+	}
+
+	if len(cmd.args) > 1 {
 		return errors.New("too many arguments provided")
 	}
 
-	rssFeed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
 
 	if err != nil {
-		return fmt.Errorf("could not fetch feed - %w", err)
+		return fmt.Errorf("couldn't parse time duration - %w", err)
 	}
 
-	fmt.Printf("%+v\n", rssFeed)
+	fmt.Printf("Collecting feeds every %s\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
 
 	return nil
+}
+
+func scrapeFeeds(s *state) {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = s.db.MarkFeedFetched(context.Background(), nextFeed.ID)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	feedData, err := fetchFeed(context.Background(), nextFeed.Url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, item := range feedData.Channel.Item {
+		fmt.Println(item.Title)
+	}
 }
