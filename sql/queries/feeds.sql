da94c77fa90a976efbda450a9b897cfa8721b877
misterlister <haydenglister@gmail.com>
2025-08-22 22:08:01 -0700

    Added functions to mark a feed as fetched, and to get the next feed to fetch

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -18,3 +18,14 @@ WHERE url = $1;
 
 -- name: ResetFeedsTable :exec
 DELETE FROM feeds;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(),
+    updated_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds 
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
